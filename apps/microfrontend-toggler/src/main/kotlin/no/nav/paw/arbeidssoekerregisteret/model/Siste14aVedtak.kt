9879c0eff78cf484273143b745546688fa8bcb8d
Thomas Johansen <thomas.johansen3@nav.no>
2024-06-06 06:57:00 +0200

    Fikset deserialiseringsfeil

--- apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/model/Siste14aVedtak.kt
+++ apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/model/Siste14aVedtak.kt
@@ -5,8 +5,8 @@ import java.time.Instant
 
 data class Siste14aVedtak(
     val aktorId: AktorId,
-    val innsatsgruppe: String, // Dette er enumererte verdier fra Innsaktsgruppe enumen
-    val hovedmal: String, // Dette er enumererte verdier fra Hovedmal enumen
+    val innsatsgruppe: String?, // Dette er enumererte verdier fra Innsaktsgruppe enumen
+    val hovedmal: String?, // Dette er enumererte verdier fra Hovedmal enumen
     val fattetDato: Instant,
     val fraArena: Boolean
 )
