a481cf31a15e3012ce1ceaac9ba260b790dd25c5
Thomas Johansen <thomas.johansen3@nav.no>
2024-12-18 08:11:40 +0100

    Bruk av felles KafkaStreamsPlugin i microfrontend-toggler

--- apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Kafka.kt
+++ apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Kafka.kt
@@ -4,12 +4,13 @@ package no.nav.paw.arbeidssoekerregisteret.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import no.nav.paw.arbeidssoekerregisteret.context.ApplicationContext
+import no.nav.paw.kafka.plugin.KafkaStreamsPlugin
 
 fun Application.configureKafka(applicationContext: ApplicationContext) {
     with(applicationContext) {
         install(KafkaStreamsPlugin) {
-            shutDownTimeout = applicationConfig.kafkaTopology.shutDownTimeout
             kafkaStreams = listOf(periodeKafkaStreams, siste14aVedtakKafkaStreams)
+            shutDownTimeout = applicationConfig.kafkaTopology.shutDownTimeout
         }
     }
 }
