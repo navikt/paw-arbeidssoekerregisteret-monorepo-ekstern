33194f5075515f59b3a625fe794518c29dc503ee
robertkittilsen <robert.graarup.kittilsen@nav.no>
2025-02-28 21:08:28 +0100

    annen måte å propagere otel context i KafkaConsumerPlugin

--- lib/kafka/src/main/kotlin/no/nav/paw/kafka/plugin/KafkaConsumerPlugin.kt
+++ lib/kafka/src/main/kotlin/no/nav/paw/kafka/plugin/KafkaConsumerPlugin.kt
@@ -7,11 +7,11 @@ import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.application.hooks.MonitoringEvent
 import io.ktor.server.application.log
 import io.opentelemetry.context.Context
-import io.opentelemetry.extension.kotlin.asContextElement
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import no.nav.paw.kafka.consumer.defaultErrorFunction
 import no.nav.paw.kafka.consumer.defaultSuccessFunction
 import no.nav.paw.kafka.listener.NoopConsumerRebalanceListener
@@ -66,18 +66,18 @@ fun <K, V> KafkaConsumerPlugin(pluginInstance: Any): ApplicationPlugin<KafkaCons
                     while (!shutdownFlag.get()) {
                         try {
                             val records = kafkaConsumer.poll(pollTimeout)
-                            val currentContext = Context.current()
-                            application.launch(coroutineDispatcher + currentContext.asContextElement()) {
-                                consumeFunction(records)
-                                successFunction(records)
+                            val parentContext = Context.current()
+                            withContext(coroutineDispatcher) {
+                                val scope = parentContext.makeCurrent()
+                                scope.use {
+                                    consumeFunction(records)
+                                    successFunction(records)
+                                }
                             }
                         } catch (throwable: Throwable) {
                             logger.info("Stopper {} Kafka Consumer", pluginInstance)
                             shutdownFlag.set(true)
-                            val currentContext = Context.current()
-                            application.launch(coroutineDispatcher + currentContext.asContextElement()) {
-                                errorFunction(throwable)
-                            }
+                            errorFunction(throwable)
                         }
                     }
                 } finally {
