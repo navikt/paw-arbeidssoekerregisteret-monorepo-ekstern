b020aa881e8639bdb382e75a3088477c77728126
Marius Eriksen <marius.eriksen@nav.no>
2025-06-10 14:59:58 +0200

    Få ut skallet av egenvurdering-api med liveness endepunkter
    Co-authored-by: robertkittilsen <robert.graarup.kittilsen@nav.no>

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
@@ -1,16 +1,26 @@
 package no.nav.paw.arbeidssoekerregisteret.plugins
 
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import no.nav.paw.arbeidssoekerregisteret.context.ApplicationContext
 import no.nav.paw.arbeidssoekerregisteret.routes.metricsRoutes
 import no.nav.paw.arbeidssoekerregisteret.routes.swaggerRoutes
-import no.nav.paw.health.route.healthRoutes
+import no.nav.paw.health.model.HealthStatus
 
 fun Application.configureRouting(applicationContext: ApplicationContext) {
     with(applicationContext) {
         routing {
-            healthRoutes(healthIndicatorRepository)
+            get("/internal/isAlive") {
+                call.respondText(ContentType.Text.Plain, HttpStatusCode.OK) { HealthStatus.HEALTHY.value }
+            }
+
+            get("/internal/isReady") {
+                call.respondText(ContentType.Text.Plain, HttpStatusCode.OK) { HealthStatus.HEALTHY.value }
+            }
             metricsRoutes(prometheusMeterRegistry)
             swaggerRoutes()
         }
