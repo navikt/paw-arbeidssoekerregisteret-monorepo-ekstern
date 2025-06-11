826bb8bd9be366efc29d1d7efdb5bf050e9bcf15
robertkittilsen <robert.graarup.kittilsen@nav.no>
2025-06-11 14:26:22 +0200

    Legger til egenvurdering POST endepunkt (flyttet fra inngangs-api)
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
@@ -7,12 +7,15 @@ import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import no.nav.paw.arbeidssoekerregisteret.context.ApplicationContext
 import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.EgenvurderingGrunnlag
+import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.EgenvurderingRequest
 import no.nav.paw.arbeidssoekerregisteret.routes.metricsRoutes
 import no.nav.paw.arbeidssoekerregisteret.routes.swaggerRoutes
+import no.nav.paw.arbeidssoekerregisteret.utils.buildApplicationLogger
 import no.nav.paw.health.model.HealthStatus
 import no.nav.paw.security.authentication.model.Sluttbruker
 import no.nav.paw.security.authentication.model.TokenX
@@ -36,13 +39,21 @@ fun Application.configureRouting(applicationContext: ApplicationContext) {
     }
 }
 
+val logger = buildApplicationLogger
+
 fun Route.egenvurderingRoutes() =
-    route("/api/v1/arbeidssoeker/profilering/egenvurdering/grunnlag") {
+    route("/api/v1/arbeidssoeker/profilering/egenvurdering") {
         autentisering(TokenX) {
-            get {
+            post<EgenvurderingRequest> { egenvurderingRequest ->
+                logger.info("Mottok egenvurderingrequest")
+                call.respond(HttpStatusCode.Accepted)
+            }
+            get("/grunnlag") {
                 val bruker = call.bruker<Sluttbruker>()
+                logger.info("Mottok grunnlagrequest")
                 call.respond(HttpStatusCode.OK, EgenvurderingGrunnlag(grunnlag = null))
             }
         }
     }
+
 
--- apps/egenvurdering-api/src/test/kotlin/no/nav/paw/arbeidssoekerregisteret/EgenvurderingRoutesTest.kt
+++ apps/egenvurdering-api/src/test/kotlin/no/nav/paw/arbeidssoekerregisteret/EgenvurderingRoutesTest.kt
@@ -6,10 +6,15 @@ import io.kotest.matchers.shouldNotBe
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType.Application
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.server.testing.testApplication
 import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.EgenvurderingGrunnlag
+import java.util.UUID
 
 class EgenvurderingRoutesTest : FreeSpec({
     with(TestContext()) {
@@ -19,6 +24,43 @@ class EgenvurderingRoutesTest : FreeSpec({
 
         afterSpec {
             mockOAuth2Server.shutdown()
+        }
+        "/api/v1/arbeidssoeker/profilering/egenvurdering" - {
+                "202 Accepted" {
+                    testApplication {
+                        configureTestApplication()
+                        val client = configureTestClient()
+                        val egenvurderingJson = """
+                            {
+                              "periodeId": "${UUID.randomUUID()}",
+                              "profileringId": "${UUID.randomUUID()}",
+                              "egenvurdering": "ANTATT_GODE_MULIGHETER"
+                            }
+                        """.trimIndent()
+
+                        val response = client.post("/api/v1/arbeidssoeker/profilering/egenvurdering") {
+                            contentType(Application.Json)
+                            setBody(egenvurderingJson)
+                            bearerAuth(mockOAuth2Server.issueTokenXToken())
+                        }
+                        response.status shouldBe HttpStatusCode.Accepted
+                        response.headers["x-trace-id"] shouldNotBe null
+                    }
+                }
+                "400 BadRequest" {
+                    testApplication {
+                        configureTestApplication()
+                        val client = configureTestClient()
+
+                        val response = client.post("/api/v1/arbeidssoeker/profilering/egenvurdering") {
+                            contentType(Application.Json)
+                            setBody("""{ugyldigJson}""")
+                            bearerAuth(mockOAuth2Server.issueTokenXToken())
+                        }
+                        response.status shouldBe HttpStatusCode.BadRequest
+                        response.headers["x-trace-id"] shouldNotBe null
+                    }
+                }
         }
         "/api/v1/arbeidssoeker/profilering/egenvurdering/grunnlag" - {
             "200 OK" {
