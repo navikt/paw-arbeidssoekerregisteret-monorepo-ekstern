7069cb72cd621d34aa1fe27a5c1d3b1d32fd63c1
Nils Martin Sande <nils.martin.sande@nav.no>
2025-08-28 07:05:18 +0200

    Revert "La til span for auth plugin kjøring"
    This reverts commit c3e67b9360e6bf891d2e9bbd20a8f32ec518c6c1.

--- lib/security/src/main/kotlin/no/nav/paw/security/authentication/plugin/AuthenticationRoutePlugin.kt
+++ lib/security/src/main/kotlin/no/nav/paw/security/authentication/plugin/AuthenticationRoutePlugin.kt
@@ -1,12 +1,9 @@
 package no.nav.paw.security.authentication.plugin
 
-import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.RouteScopedPlugin
 import io.ktor.server.application.createRouteScopedPlugin
 import io.ktor.server.application.log
 import io.ktor.server.auth.AuthenticationChecked
-import io.opentelemetry.api.trace.SpanKind.INTERNAL
-import io.opentelemetry.instrumentation.annotations.WithSpan
 import no.nav.paw.security.authentication.model.SecurityContext
 import no.nav.paw.security.authentication.model.resolveSecurityContext
 import no.nav.paw.security.authentication.model.securityContext
@@ -33,19 +30,8 @@ val AuthenticationRoutePlugin
         val modifyPrincipal = pluginConfig.modifyPrincipal ?: { it }
 
         on(AuthenticationChecked) { call ->
-            authenticate(modifyPrincipal, call)
+            logger.trace("Kjører autentisering")
+            val securityContext = modifyPrincipal(call.resolveSecurityContext())
+            call.securityContext(securityContext)
         }
     }
-
-@WithSpan(
-    value = "authenticate_incoming",
-    kind = INTERNAL
-)
-private suspend fun authenticate(
-    modifyPrincipal: suspend (SecurityContext) -> SecurityContext,
-    call: ApplicationCall
-) {
-    logger.trace("Kjører autentisering")
-    val securityContext = modifyPrincipal(call.resolveSecurityContext())
-    call.securityContext(securityContext)
-}
