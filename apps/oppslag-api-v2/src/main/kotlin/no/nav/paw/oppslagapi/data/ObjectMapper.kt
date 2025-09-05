c7284e16c110ccb296b44140a27c7aa6b565dff9
Nils Martin Sande <nils.martin.sande@nav.no>
2025-09-05 07:18:03 +0200

    Fjærnet jackson cfg som ble overskrevet 2 linjer lenger nede

--- apps/oppslag-api-v2/src/main/kotlin/no/nav/paw/oppslagapi/data/ObjectMapper.kt
+++ apps/oppslag-api-v2/src/main/kotlin/no/nav/paw/oppslagapi/data/ObjectMapper.kt
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 val objectMapper = ObjectMapper()
     .registerKotlinModule()
-    .enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
     .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     .registerModule(JavaTimeModule())
