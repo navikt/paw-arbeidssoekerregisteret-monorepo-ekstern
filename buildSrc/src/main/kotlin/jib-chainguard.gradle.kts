e51d7a567e55b45b922535aaf3cf5e61d9f1a7fd
Marius Eriksen <marius.eriksen@nav.no>
2025-09-08 12:24:28 +0200

    Bruk prosjektet sin jvmMajorVersion i bygging av chainguard image
    Co-authored-by: Nils Martin Sande <nils.martin.sande@nav.no>

--- buildSrc/src/main/kotlin/jib-chainguard.gradle.kts
+++ buildSrc/src/main/kotlin/jib-chainguard.gradle.kts
@@ -3,11 +3,12 @@ plugins {
 }
 
 val chainguardJavaImage : String by project
+val jvmMajorVersion: String by project
 val image: String? by project
 val targetImage: String = "${image ?: project.name}:${project.version}"
 
 jib {
-    from.image = chainguardJavaImage
+    from.image = "$chainguardJavaImage$jvmMajorVersion"
     to.image = targetImage
     container {
         jvmFlags = listOf("-XX:ActiveProcessorCount=8", "-XX:+UseZGC", "-XX:+ZGenerational")
