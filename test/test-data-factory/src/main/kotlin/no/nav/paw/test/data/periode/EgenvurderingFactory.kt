d54549521d4dace7c15dd15f279bbb190a4421b6
Marius Eriksen <marius.eriksen@nav.no>
2025-09-15 18:04:15 +0200

    Legg til hjelpemetoder for å opprette Egenvurdering

---
+++ test/test-data-factory/src/main/kotlin/no/nav/paw/test/data/periode/EgenvurderingFactory.kt
@@ -0,0 +1,41 @@
+package no.nav.paw.test.data.periode
+
+import no.nav.paw.arbeidssokerregisteret.api.v1.Metadata
+import no.nav.paw.arbeidssokerregisteret.api.v1.Profilering
+import no.nav.paw.arbeidssokerregisteret.api.v1.ProfilertTil
+import no.nav.paw.arbeidssokerregisteret.api.v2.Egenvurdering
+import java.util.*
+
+fun createEgenvurdering(
+    id: UUID = UUID.randomUUID(),
+    periodeId: UUID = UUID.randomUUID(),
+    opplysningerId: UUID = UUID.randomUUID(),
+    profileringId: UUID = UUID.randomUUID(),
+    sendtInnAv: Metadata = MetadataFactory.create().build(),
+    profilertTil: ProfilertTil = ProfilertTil.ANTATT_GODE_MULIGHETER,
+    egenvurdering: ProfilertTil = ProfilertTil.ANTATT_GODE_MULIGHETER,
+) = Egenvurdering(
+    id,
+    periodeId,
+    opplysningerId,
+    profileringId,
+    sendtInnAv,
+    profilertTil,
+    egenvurdering
+)
+
+fun createEgenvurderingFor(
+    profilering: Profilering,
+    id: UUID = UUID.randomUUID(),
+    sendtInnAv: Metadata = MetadataFactory.create().build(),
+    profilertTil: ProfilertTil = profilering.profilertTil,
+    egenvurdering: ProfilertTil = ProfilertTil.ANTATT_GODE_MULIGHETER,
+) = Egenvurdering(
+    id,
+    profilering.periodeId,
+    profilering.opplysningerOmArbeidssokerId,
+    profilering.id,
+    sendtInnAv,
+    profilertTil,
+    egenvurdering
+)
