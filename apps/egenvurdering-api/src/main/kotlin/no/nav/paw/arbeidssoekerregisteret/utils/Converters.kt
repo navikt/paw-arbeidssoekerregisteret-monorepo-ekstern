c4da2cf63598086255fd90c5ee4a595b91bd39f3
Marius Eriksen <marius.eriksen@nav.no>
2025-09-17 11:01:17 +0200

    Fjern ubrukt kode

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Converters.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Converters.kt
@@ -1,22 +1,13 @@
 package no.nav.paw.arbeidssoekerregisteret.utils
 
-import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.Egenvurdering as ApiEgenvurdering
-import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.Profilering as ApiProfilering
-import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.ProfilertTil as ApiProfilertTil
 import no.nav.paw.arbeidssokerregisteret.api.v1.ProfilertTil
 import no.nav.paw.client.api.oppslag.models.ArbeidssoekerperiodeAggregertResponse
 import no.nav.paw.client.api.oppslag.models.OpplysningerOmArbeidssoekerAggregertResponse
-import no.nav.paw.client.api.oppslag.models.ProfileringAggregertResponse
 import no.nav.paw.client.api.oppslag.models.ProfileringsResultat
-
-fun List<ArbeidssoekerperiodeAggregertResponse>.findSisteProfilering(): ProfileringAggregertResponse? =
-    this.firstOrNull()?.opplysningerOmArbeidssoeker?.maxByOrNull { it.sendtInnAv.tidspunkt }?.profilering
+import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.Egenvurdering as ApiEgenvurdering
 
 fun ArbeidssoekerperiodeAggregertResponse.findSisteOpplysningerOmArbeidssoeker(): OpplysningerOmArbeidssoekerAggregertResponse? =
     this.opplysningerOmArbeidssoeker?.maxByOrNull { it.sendtInnAv.tidspunkt }
-
-fun List<ArbeidssoekerperiodeAggregertResponse>.isPeriodeAvsluttet(): Boolean =
-    this.isNotEmpty() && this[0].avsluttet != null
 
 fun ProfileringsResultat.toProfilertTil(): ProfilertTil {
     return when (this) {
@@ -33,18 +24,3 @@ fun ApiEgenvurdering.toProfilertTil(): ProfilertTil =
         ApiEgenvurdering.ANTATT_BEHOV_FOR_VEILEDNING -> ProfilertTil.ANTATT_BEHOV_FOR_VEILEDNING
         ApiEgenvurdering.OPPGITT_HINDRINGER -> ProfilertTil.OPPGITT_HINDRINGER
     }
-
-fun ProfileringAggregertResponse.toApiProfilering(): ApiProfilering =
-    ApiProfilering(
-        profileringId = profileringId,
-        profilertTil = profilertTil.toApiProfilertTil() ?: throw IllegalArgumentException("Ugyldig profilertTil: $profilertTil"),
-    )
-
-fun ProfileringsResultat.toApiProfilertTil(): ApiProfilertTil? {
-    return when (this) {
-        ProfileringsResultat.ANTATT_GODE_MULIGHETER -> ApiProfilertTil.ANTATT_GODE_MULIGHETER
-        ProfileringsResultat.ANTATT_BEHOV_FOR_VEILEDNING -> ApiProfilertTil.ANTATT_BEHOV_FOR_VEILEDNING
-        ProfileringsResultat.OPPGITT_HINDRINGER -> ApiProfilertTil.OPPGITT_HINDRINGER
-        else -> null
-    }
-}
