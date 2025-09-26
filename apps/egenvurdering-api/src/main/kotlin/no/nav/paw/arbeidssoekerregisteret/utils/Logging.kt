db46e3b3fef314b25af519035a6a64880febe979
Marius Eriksen <marius.eriksen@nav.no>
2025-09-26 11:09:01 +0200

    Fjern ubrukt kode

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Logging.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Logging.kt
@@ -3,6 +3,4 @@ package no.nav.paw.arbeidssoekerregisteret.utils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-inline val <reified T> T.buildLogger: Logger get() = LoggerFactory.getLogger(T::class.java)
-
 inline val buildApplicationLogger: Logger get() = LoggerFactory.getLogger("no.nav.paw.logger.application")
