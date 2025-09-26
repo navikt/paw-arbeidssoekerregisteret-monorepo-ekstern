b9d6c804420a7b886a7a7de301f57a1a460862e4
Marius Eriksen <marius.eriksen@nav.no>
2025-09-26 11:19:42 +0200

    Map fra String til ProfilertTil

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Converters.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/utils/Converters.kt
@@ -1,19 +1,13 @@
 package no.nav.paw.arbeidssoekerregisteret.utils
 
 import no.nav.paw.arbeidssokerregisteret.api.v1.ProfilertTil
-import no.nav.paw.client.api.oppslag.models.ArbeidssoekerperiodeAggregertResponse
-import no.nav.paw.client.api.oppslag.models.OpplysningerOmArbeidssoekerAggregertResponse
-import no.nav.paw.client.api.oppslag.models.ProfileringsResultat
 import no.nav.paw.arbeidssoekerregisteret.egenvurdering.api.models.Egenvurdering as ApiEgenvurdering
 
-fun ArbeidssoekerperiodeAggregertResponse.findSisteOpplysningerOmArbeidssoeker(): OpplysningerOmArbeidssoekerAggregertResponse? =
-    this.opplysningerOmArbeidssoeker?.maxByOrNull { it.sendtInnAv.tidspunkt }
-
-fun ProfileringsResultat.toProfilertTil(): ProfilertTil {
+fun String.toProfilertTil(): ProfilertTil {
     return when (this) {
-        ProfileringsResultat.ANTATT_GODE_MULIGHETER -> ProfilertTil.ANTATT_GODE_MULIGHETER
-        ProfileringsResultat.ANTATT_BEHOV_FOR_VEILEDNING -> ProfilertTil.ANTATT_BEHOV_FOR_VEILEDNING
-        ProfileringsResultat.OPPGITT_HINDRINGER -> ProfilertTil.OPPGITT_HINDRINGER
+        "ANTATT_GODE_MULIGHETER" -> ProfilertTil.ANTATT_GODE_MULIGHETER
+        "ANTATT_BEHOV_FOR_VEILEDNING" -> ProfilertTil.ANTATT_BEHOV_FOR_VEILEDNING
+        "OPPGITT_HINDRINGER" -> ProfilertTil.OPPGITT_HINDRINGER
         else -> throw IllegalArgumentException("Ugyldig profilertTil: $this")
     }
 }
