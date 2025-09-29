1a857603a1c390ee145d636b1ee812b842f38a4b
Marius Eriksen <marius.eriksen@nav.no>
2025-09-29 11:12:15 +0200

    Legg til mulighet for å konfigurere server prepared statements

--- lib/database-v2/src/main/kotlin/no/nav/paw/database/factory/DataSourceFactory.kt
+++ lib/database-v2/src/main/kotlin/no/nav/paw/database/factory/DataSourceFactory.kt
@@ -4,14 +4,18 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import no.nav.paw.database.config.DatabaseConfig
 
-fun createHikariDataSource(databaseConfig: DatabaseConfig): HikariDataSource =
-    HikariDataSource(
-        HikariConfig().apply {
-            jdbcUrl = databaseConfig.buildJdbcUrl()
-            maximumPoolSize = databaseConfig.maximumPoolSize
-            isAutoCommit = databaseConfig.autoCommit
-            connectionTimeout = databaseConfig.connectionTimeout.toMillis()
-            idleTimeout = databaseConfig.idleTimeout.toMillis()
-            maxLifetime = databaseConfig.maxLifetime.toMillis()
-        }
-    )
+fun createHikariDataSource(
+    databaseConfig: DatabaseConfig,
+    useServerPreparedStatements: Boolean = true,
+) = HikariDataSource(
+    HikariConfig().apply {
+        jdbcUrl = databaseConfig.buildJdbcUrl()
+        maximumPoolSize = databaseConfig.maximumPoolSize
+        isAutoCommit = databaseConfig.autoCommit
+        connectionTimeout = databaseConfig.connectionTimeout.toMillis()
+        idleTimeout = databaseConfig.idleTimeout.toMillis()
+        maxLifetime = databaseConfig.maxLifetime.toMillis()
+
+        if (!useServerPreparedStatements) addDataSourceProperty("prepareThreshold", "0")
+    }
+)
