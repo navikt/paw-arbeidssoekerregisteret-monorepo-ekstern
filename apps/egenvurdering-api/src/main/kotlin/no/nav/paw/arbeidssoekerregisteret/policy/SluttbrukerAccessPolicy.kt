02791f9b3478345d2e1b7de3e9fd29362b9e3943
Marius Eriksen <marius.eriksen@nav.no>
2025-10-01 12:18:10 +0200

    Forenkle SluttbrukerAccessPolicy

--- apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/policy/SluttbrukerAccessPolicy.kt
+++ apps/egenvurdering-api/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/policy/SluttbrukerAccessPolicy.kt
@@ -9,18 +9,11 @@ import no.nav.paw.security.authorization.model.Permit
 import no.nav.paw.security.authorization.policy.AccessPolicy
 
 class SluttbrukerAccessPolicy : AccessPolicy {
-
     override suspend fun hasAccess(action: Action, securityContext: SecurityContext): AccessDecision {
-        val (bruker, _) = securityContext
+        return when (val bruker = securityContext.bruker) {
+            is Sluttbruker -> Permit("Sluttbruker har tilgang")
 
-        return when (bruker) {
-            is Sluttbruker -> {
-                Permit("Sluttbruker har tilgang")
-            }
-
-            else -> {
-                Deny("Ingen tilgang for brukergruppe ${bruker::class.simpleName}")
-            }
+            else -> Deny("Ingen tilgang for brukergruppe ${bruker::class.simpleName}")
         }
     }
 }
