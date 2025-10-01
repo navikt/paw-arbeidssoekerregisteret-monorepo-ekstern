4f10e920e8e6e9e23b0a1499b850d21d3bb49877
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-01 14:51:55 +0200

    Fikset manglende isReady endepunkt

--- apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
+++ apps/microfrontend-toggler/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/plugins/Routing.kt
@@ -7,11 +7,13 @@ import no.nav.paw.arbeidssoekerregisteret.routes.metricsRoutes
 import no.nav.paw.arbeidssoekerregisteret.routes.swaggerRoutes
 import no.nav.paw.arbeidssoekerregisteret.routes.toggleRoutes
 import no.nav.paw.health.liveness.livenessRoute
+import no.nav.paw.health.readiness.readinessRoute
 
 fun Application.configureRouting(applicationContext: ApplicationContext) {
     with(applicationContext) {
         routing {
             livenessRoute(healthChecks)
+            readinessRoute(healthChecks)
             metricsRoutes(prometheusMeterRegistry)
             swaggerRoutes()
             toggleRoutes(applicationConfig, authorizationService, toggleService)
