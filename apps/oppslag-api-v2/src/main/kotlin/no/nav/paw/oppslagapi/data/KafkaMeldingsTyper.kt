2022a2fe86113fe3e1d68323822edc1eb2f87e97
Marius Eriksen <marius.eriksen@nav.no>
2025-10-07 12:58:26 +0200

    Legg til egenvurdering som kafka meldingstype

--- apps/oppslag-api-v2/src/main/kotlin/no/nav/paw/oppslagapi/data/KafkaMeldingsTyper.kt
+++ apps/oppslag-api-v2/src/main/kotlin/no/nav/paw/oppslagapi/data/KafkaMeldingsTyper.kt
@@ -6,6 +6,7 @@ import no.nav.paw.arbeidssoekerregisteret.api.v2.oppslag.models.OpplysningerOmAr
 import no.nav.paw.arbeidssoekerregisteret.api.v2.oppslag.models.PaaVegneAvStart
 import no.nav.paw.arbeidssoekerregisteret.api.v2.oppslag.models.PaaVegneAvStopp
 import no.nav.paw.arbeidssoekerregisteret.api.v2.oppslag.models.Profilering
+import no.nav.paw.arbeidssokerregisteret.api.v3.Egenvurdering
 
 const val periode_startet_v1 = "periode_startet-v1"
 const val periode_avsluttet_v1 = "periode_avsluttet-v1"
@@ -14,6 +15,7 @@ const val profilering_v1 = "profilering-v1"
 const val bekreftelsemelding_v1 = "bekreftelse-v1"
 const val pa_vegne_av_start_v1 = "pa_vegne_av_start-v1"
 const val pa_vegne_av_stopp_v1 = "pa_vegne_av_stopp-v1"
+const val egenvurdering_v1 = "egenvurdering-v1"
 
 val typeTilKlasse = mapOf(
     periode_startet_v1 to Metadata::class,
@@ -22,5 +24,6 @@ val typeTilKlasse = mapOf(
     profilering_v1 to Profilering::class,
     bekreftelsemelding_v1 to Bekreftelse::class,
     pa_vegne_av_start_v1 to PaaVegneAvStart::class,
-    pa_vegne_av_stopp_v1 to PaaVegneAvStopp::class
+    pa_vegne_av_stopp_v1 to PaaVegneAvStopp::class,
+    egenvurdering_v1 to Egenvurdering::class,
 )
