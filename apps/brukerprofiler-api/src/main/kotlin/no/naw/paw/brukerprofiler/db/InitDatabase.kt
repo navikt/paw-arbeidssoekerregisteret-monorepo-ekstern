d56ebb27f7ffdf788835d9f0dbd4a7b751a1a34c
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-10 12:15:10 +0200

    La til auto-clean av db migrations ved feil (bare i dev)

--- apps/brukerprofiler-api/src/main/kotlin/no/naw/paw/brukerprofiler/db/InitDatabase.kt
+++ apps/brukerprofiler-api/src/main/kotlin/no/naw/paw/brukerprofiler/db/InitDatabase.kt
@@ -1,6 +1,10 @@
 package no.naw.paw.brukerprofiler.db
 
 import com.zaxxer.hikari.HikariDataSource
+import no.nav.paw.config.env.Nais
+import no.nav.paw.config.env.ProdGcp
+import no.nav.paw.config.env.RuntimeEnvironment
+import no.nav.paw.config.env.currentRuntimeEnvironment
 import no.nav.paw.database.config.DatabaseConfig
 import no.nav.paw.database.factory.createHikariDataSource
 import org.flywaydb.core.Flyway
@@ -9,12 +13,19 @@ import org.jetbrains.exposed.v1.jdbc.Database
 
 fun initDatabase(databaseConfig: DatabaseConfig): HikariDataSource {
     val dataSource = createHikariDataSource(databaseConfig)
+    val disableClean = false
+    val cleanOnValidationError = true
     Database.connect(dataSource)
+    if (currentRuntimeEnvironment is ProdGcp ) {
+        if (!disableClean) throw IllegalStateException("Cannot run with clean enabled in ProdGcp")
+        if (cleanOnValidationError) throw IllegalStateException("Cannot run with cleanOnValidationError enabled in ProdGcp")
+    }
     Flyway.configure()
         .dataSource(dataSource)
         .baselineOnMigrate(true)
         .locations("db/migration")
-        .cleanDisabled(false)
+        .cleanDisabled(disableClean)
+        .cleanOnValidationError(cleanOnValidationError)
         .load()
         .migrate()
     return dataSource
