bdf9b803dafa672304edde14ef1a1e476667dffa
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-10 10:51:31 +0200

    La til m2m token client som input param og fjernet runtime env param.
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- lib/pdl-client/src/main/kotlin/no/nav/paw/pdl/factory/PdlClientFactory.kt
+++ lib/pdl-client/src/main/kotlin/no/nav/paw/pdl/factory/PdlClientFactory.kt
@@ -1,11 +1,11 @@
 package no.nav.paw.pdl.factory
 
 import io.ktor.client.HttpClient
+import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient
 import no.nav.paw.client.config.AZURE_M2M_CONFIG
 import no.nav.paw.client.config.AzureAdM2MConfig
 import no.nav.paw.client.factory.createAzureAdM2MTokenClient
 import no.nav.paw.client.factory.createHttpClient
-import no.nav.paw.config.env.RuntimeEnvironment
 import no.nav.paw.config.env.currentRuntimeEnvironment
 import no.nav.paw.config.hoplite.loadNaisOrLocalConfiguration
 import no.nav.paw.pdl.client.PdlClient
@@ -14,13 +14,12 @@ import no.nav.paw.pdl.config.PdlClientConfig
 
 fun createPdlClient(
     httpClient: HttpClient = createHttpClient(),
-    runtimeEnvironment: RuntimeEnvironment = currentRuntimeEnvironment
-): PdlClient {
-    val pdlClientConfig = loadNaisOrLocalConfiguration<PdlClientConfig>(PDL_CLIENT_CONFIG)
-    val azureAdM2MTokenClient = createAzureAdM2MTokenClient(
-        runtimeEnvironment,
+    azureAdM2MTokenClient: AzureAdMachineToMachineTokenClient = createAzureAdM2MTokenClient(
+        currentRuntimeEnvironment,
         loadNaisOrLocalConfiguration<AzureAdM2MConfig>(AZURE_M2M_CONFIG)
     )
+): PdlClient {
+    val pdlClientConfig = loadNaisOrLocalConfiguration<PdlClientConfig>(PDL_CLIENT_CONFIG)
     return PdlClient(
         url = pdlClientConfig.url,
         tema = pdlClientConfig.tema,
