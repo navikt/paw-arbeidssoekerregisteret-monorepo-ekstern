4945774e23234f10ef2238ffbf62f1f1f04a92fb
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-13 06:38:25 +0200

    La til støtte for bulk henting av adressebeskyttelse

--- lib/pdl-client/src/main/kotlin/no/nav/paw/pdl/client/HentAdressebeskyttelse.kt
+++ lib/pdl-client/src/main/kotlin/no/nav/paw/pdl/client/HentAdressebeskyttelse.kt
@@ -3,7 +3,9 @@ package no.nav.paw.pdl.client
 import no.nav.paw.pdl.exception.PdlPersonIkkeFunnetException
 import no.nav.paw.pdl.exception.PdlUkjentFeilException
 import no.nav.paw.pdl.graphql.generated.HentAdressebeskyttelse
+import no.nav.paw.pdl.graphql.generated.HentAdressebeskyttelseBolk
 import no.nav.paw.pdl.graphql.generated.hentadressebeskyttelse.Adressebeskyttelse
+import no.nav.paw.pdl.graphql.generated.hentadressebeskyttelsebolk.HentPersonBolkResult
 
 suspend fun PdlClient.hentAdressebeskyttelse(
     ident: String,
@@ -30,3 +32,28 @@ suspend fun PdlClient.hentAdressebeskyttelse(
     logger.debug("Hentet opphold fra PDL")
     return response.data?.hentPerson?.adressebeskyttelse
 }
+suspend fun PdlClient.hentAdressebeskyttelse(
+    identer: List<String>,
+    navConsumerId: String?,
+    behandlingsnummer: String
+): List <HentPersonBolkResult>? {
+    val query = HentAdressebeskyttelseBolk(HentAdressebeskyttelseBolk.Variables(identer = identer))
+    logger.debug("Henter adressebeskyttelse fra PDL")
+    val response = execute(
+        query = query,
+        callId = null,
+        navConsumerId = navConsumerId,
+        behandlingsnummer = behandlingsnummer
+    )
+    response.errors?.let {
+        logger.debug("Hent adressebeskyttelse fra PDL feilet med: {}", it)
+        if (response.hasNotFoundError()) {
+            throw PdlPersonIkkeFunnetException("Person ikke funnet i PDL", it)
+        } else {
+            throw PdlUkjentFeilException("Kall til PDL feilet", it)
+        }
+        logger.debug("Hentet opphold fra PDL")
+    }
+    return response.data?.hentPersonBolk
+}
+
