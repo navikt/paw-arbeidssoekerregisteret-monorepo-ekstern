8f12b68491c84673558f20603f523856e6b680de
Marius Eriksen <marius.eriksen@nav.no>
2025-10-15 14:21:38 +0200

    Script for henting av fylker, kommuner og styrk koder fra SSB, samt enkle dataklasser
    Co-authored-by: Nils Martin Sande <nils.martin.sande@nav.no>

---
+++ apps/brukerprofiler-api/hentStyrkKoderFylkerOgKommuner.sh
@@ -0,0 +1,3 @@
+wget -O src/main/resources/styrk.json https://data.ssb.no/api/klass/v1/classifications/7/codes?from=2024-01-01
+wget -O src/main/resources/fylke.json https://data.ssb.no/api/klass/v1/classifications/104/codes?from=2024-01-01
+wget -O src/main/resources/kommune.json https://data.ssb.no/api/klass/v1/classifications/131/codes?from=2024-01-01
