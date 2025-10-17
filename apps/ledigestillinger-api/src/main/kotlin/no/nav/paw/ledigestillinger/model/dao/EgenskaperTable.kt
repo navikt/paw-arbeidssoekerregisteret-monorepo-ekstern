36bc9e524997168f8da276265e241d9325c0ecd3
Thomas Johansen <thomas.johansen3@nav.no>
2025-10-17 09:46:06 +0200

    Endret kolonne-lengde

--- apps/ledigestillinger-api/src/main/kotlin/no/nav/paw/ledigestillinger/model/dao/EgenskaperTable.kt
+++ apps/ledigestillinger-api/src/main/kotlin/no/nav/paw/ledigestillinger/model/dao/EgenskaperTable.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.v1.jdbc.update
 object EgenskaperTable : LongIdTable("egenskaper") {
     val parentId = long("parent_id").references(StillingerTable.id)
     val key = varchar("key", 50)
-    val value = varchar("value", 255)
+    val value = varchar("value", 1000)
 }
 
 fun EgenskaperTable.insert(
