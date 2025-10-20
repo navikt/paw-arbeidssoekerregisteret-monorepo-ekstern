19b7f0691a794922de3b20f3f919ece19dc8ed74
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-20 09:08:50 +0200

    Initielt utkast for henting av ledige stillinger

--- apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/api/ApiLedigeStillinger.kt
+++ apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/api/ApiLedigeStillinger.kt
@@ -1,6 +1,8 @@
 package no.naw.paw.minestillinger.api
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.Instant
+import java.time.LocalDate
 
 data class ApiLedigeStillinger(
     val soek: List<ApiStillingssoek>,
@@ -11,7 +13,24 @@ data class JobbAnnonse(
     val tittel: String,
     val stillingbeskrivelse: String?,
     val publisert: Instant,
-    val soeknadsfrist: Instant?,
+    val soeknadsfrist: Soeknadsfrist,
     val land: String,
-    val kommune: String?
+    val kommune: String?,
+    val sektor: Sektor,
+    val selskap: String
 )
+
+data class Soeknadsfrist(
+    val raw: String,
+    @JsonProperty("fristType")
+    val type: SoeknadsfristType,
+    val dato: LocalDate?
+)
+
+enum class SoeknadsfristType {
+    Ukjent, Snarest, Dato
+}
+
+enum class Sektor {
+    Offentlig, Privat, Ukjent
+}
