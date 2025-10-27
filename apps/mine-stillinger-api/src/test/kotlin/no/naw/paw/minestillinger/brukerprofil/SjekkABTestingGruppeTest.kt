c30fc95a492df3bb94ef4cf48384dd10d46b3767
Nils Martin Sande <nils.martin.sande@nav.no>
2025-10-27 12:15:38 +0100

    Fikset regex for av testing i dev

---
+++ apps/mine-stillinger-api/src/test/kotlin/no/naw/paw/minestillinger/brukerprofil/SjekkABTestingGruppeTest.kt
@@ -0,0 +1,68 @@
+package no.naw.paw.minestillinger.brukerprofil
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import no.nav.paw.model.Identitetsnummer
+
+
+class SjekkABTestingGruppeTest : FreeSpec({
+    val matchTestGruppen = "\\d([02468])\\d{9}"
+    val ikkeMatchNoen = "[az]{100}"
+
+    "Regex '${matchTestGruppen} skal matche følgende identitetsnummer" - {
+        val regex = Regex(matchTestGruppen)
+        listOf(
+            "12012345678",
+            "34098765432",
+            "56011122334",
+            "78099988877",
+            "90055544433",
+            "34333333333"
+        ).map(::Identitetsnummer).forEach { identitetsnummer ->
+            "skal matche ${identitetsnummer.verdi}, lengde=${identitetsnummer.verdi.length}" {
+                sjekkABTestingGruppe(regex, identitetsnummer) shouldBe true
+            }
+        }
+    }
+
+    "Regex '${matchTestGruppen} skal ikke matche følgende identitetsnummer" - {
+        val regex = Regex(matchTestGruppen)
+        listOf(
+            "11312345678",
+            "35198765432",
+            "57111122334",
+            "79199988877",
+            "91155544433",
+            "33333333337",
+            "23222222222",
+            "2a222222222"
+        ).map(::Identitetsnummer).forEach { identitetsnummer ->
+            "skal ikke matche ${identitetsnummer.verdi}" {
+                sjekkABTestingGruppe(regex, identitetsnummer) shouldBe false
+            }
+        }
+    }
+    "Regex '${ikkeMatchNoen} skal ikke matche noen identitetsnummer" - {
+        val regex = Regex(ikkeMatchNoen)
+        listOf(
+            "12012345678",
+            "34098765432",
+            "56011122334",
+            "78099988877",
+            "90055544433",
+            "34333333333",
+            "11312345678",
+            "35198765432",
+            "57111122334",
+            "79199988877",
+            "91155544433",
+            "33333333337",
+            "23222222222",
+            "2a222222222"
+        ).map(::Identitetsnummer).forEach { identitetsnummer ->
+            "skal ikke matche ${identitetsnummer.verdi}" {
+                sjekkABTestingGruppe(regex, identitetsnummer) shouldBe false
+            }
+        }
+    }
+})
