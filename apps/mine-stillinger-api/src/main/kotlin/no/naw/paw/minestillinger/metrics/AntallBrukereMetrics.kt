f239f85981370493924589cf3964ddb0ef600e23
Nils Martin Sande <nils.martin.sande@nav.no>
2025-11-11 10:30:01 +0100

    La til telling for om tjenesten har vært brukt tidligere

--- apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/metrics/AntallBrukereMetrics.kt
+++ apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/metrics/AntallBrukereMetrics.kt
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
 import kotlinx.coroutines.delay
 import no.naw.paw.minestillinger.appLogger
+import no.naw.paw.minestillinger.brukerprofil.flagg.HarBruktTjenestenFlaggtype
 import no.naw.paw.minestillinger.brukerprofil.flagg.OptOutFlaggtype
 import no.naw.paw.minestillinger.brukerprofil.flagg.TjenestenErAktivFlaggtype
 import no.naw.paw.minestillinger.db.BrukerFlaggTable
@@ -45,6 +46,7 @@ class AntallBrukereMetrics(
                 appLogger.info("debugInfoAntallProfiler: $debugInfoAntallProfiler")
                 appLogger.info("debugInfoAntallAktivePerioder: $debuigInfoAntallAktivePerioder")
                 val optOut = BrukerFlaggTable.alias("opt_out")
+                val harBruktTjenesten = BrukerFlaggTable.alias("har_brukt_tjenesten")
                 BrukerTable
                     .join(
                         otherTable = BrukerFlaggTable,
@@ -57,6 +59,11 @@ class AntallBrukereMetrics(
                         onColumn = BrukerTable.id,
                         otherColumn = optOut[BrukerFlaggTable.brukerId],
                     ).join(
+                        otherTable = harBruktTjenesten,
+                        joinType = JoinType.LEFT,
+                        onColumn = BrukerTable.id,
+                        otherColumn = harBruktTjenesten[BrukerFlaggTable.brukerId],
+                    ).join(
                         otherTable = ProfileringTable,
                         joinType = JoinType.LEFT,
                         onColumn = BrukerTable.arbeidssoekerperiodeId,
@@ -66,19 +73,24 @@ class AntallBrukereMetrics(
                         BrukerFlaggTable.verdi,
                         ProfileringTable.profileringResultat,
                         optOut[BrukerFlaggTable.verdi],
+                        harBruktTjenesten[BrukerFlaggTable.verdi],
                         BrukerTable.id.count(),
                     ).where {
-                        BrukerFlaggTable.navn eq TjenestenErAktivFlaggtype.type and (optOut[BrukerFlaggTable.navn] eq OptOutFlaggtype.type)
+                        BrukerFlaggTable.navn eq TjenestenErAktivFlaggtype.type and (optOut[BrukerFlaggTable.navn] eq OptOutFlaggtype.type) and (
+                            harBruktTjenesten[BrukerFlaggTable.navn] eq HarBruktTjenestenFlaggtype.type
+                        )
                     }.groupBy(
                         BrukerTable.arbeidssoekerperiodeAvsluttet,
                         BrukerFlaggTable.verdi,
                         ProfileringTable.profileringResultat,
                         optOut[BrukerFlaggTable.verdi],
+                        harBruktTjenesten[BrukerFlaggTable.verdi],
                     ).map { row ->
                         MetricDataKey(
                             arbeidssoekerPeriodenErAktiv = row[BrukerTable.arbeidssoekerperiodeAvsluttet] == null,
                             tjenestenErAktiv = row[BrukerFlaggTable.verdi] == true,
                             optOut = row[optOut[BrukerFlaggTable.verdi]] == true,
+                            harBruktTjenesten = row[harBruktTjenesten[BrukerFlaggTable.verdi]] == true,
                             profileringsResultat =
                                 row[ProfileringTable.profileringResultat]
                                     ?.let { ProfileringResultat.valueOf(it) }
@@ -112,6 +124,7 @@ class AntallBrukereMetrics(
                                 Tag.of("tjenesten_er_aktiv", key.tjenestenErAktiv.toString()),
                                 Tag.of("profilerings_resultat", key.profileringsResultat.name),
                                 Tag.of("opt_out", key.optOut.toString()),
+                                Tag.of("har_brukt_tjenesten", key.harBruktTjenesten.toString()),
                             ),
                             atomicLong,
                             { it.get().toDouble() },
@@ -128,6 +141,7 @@ data class MetricDataKey(
     val arbeidssoekerPeriodenErAktiv: Boolean,
     val tjenestenErAktiv: Boolean,
     val optOut: Boolean,
+    val harBruktTjenesten: Boolean,
     val profileringsResultat: ProfileringResultat,
 )
 
