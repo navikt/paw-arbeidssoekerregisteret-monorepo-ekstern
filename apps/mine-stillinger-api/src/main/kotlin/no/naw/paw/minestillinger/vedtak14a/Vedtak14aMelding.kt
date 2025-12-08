8c571b44acced6de66981f2d2e3cca449ddf9ed8
Nils Martin Sande <nils.martin.sande@nav.no>
2025-12-08 12:41:37 +0100

    La til dataobjekter for 14a

---
+++ apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/vedtak14a/Vedtak14aMelding.kt
@@ -0,0 +1,38 @@
+package no.naw.paw.minestillinger.vedtak14a
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.paw.felles.model.AktorId
+import org.apache.kafka.common.serialization.Deserializer
+import java.time.ZonedDateTime
+
+class Siste14aVedtakMelding {
+    var aktorId: AktorId? = null
+    var innsatsgruppe: Innsatsgruppe? = null
+    var hovedmal: Hovedmål? = null
+    var fattetDato: ZonedDateTime? = null
+    var fraArena: Boolean = false
+}
+
+data class AktørId(val id: String)
+
+enum class Hovedmål {
+    SKAFFE_ARBEID,
+    BEHOLDE_ARBEID,
+    OKE_DELTAKELSE
+}
+
+enum class Innsatsgruppe {
+    STANDARD_INNSATS,
+    SITUASJONSBESTEMT_INNSATS,
+    SPESIELT_TILPASSET_INNSATS,
+    GRADERT_VARIG_TILPASSET_INNSATS,
+    VARIG_TILPASSET_INNSATS
+}
+
+object Siste14aDeserializer: Deserializer<Siste14aVedtakMelding> {
+    val objectMapper = ObjectMapper()
+    override fun deserialize(topic: String?, data: ByteArray?): Siste14aVedtakMelding? {
+        if (data == null) return null
+        return objectMapper.readValue(data, Siste14aVedtakMelding::class.java)
+    }
+}
