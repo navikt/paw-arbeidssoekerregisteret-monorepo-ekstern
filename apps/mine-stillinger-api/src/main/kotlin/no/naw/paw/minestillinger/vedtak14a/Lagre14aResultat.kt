051d7370872b22da49166a2478bf0d8becf385ed
Nils Martin Sande <nils.martin.sande@nav.no>
2025-12-11 14:28:46 +0100

    Oppdaterte lagre14Vedtak funksjonen til å bare ta inn en melding

--- apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/vedtak14a/Lagre14aResultat.kt
+++ apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/vedtak14a/Lagre14aResultat.kt
@@ -16,45 +16,42 @@ import java.time.Instant
 fun lagre14aResultat(
     idFunction: (AktorId) -> Identitetsnummer?,
     brukerprofilTjeneste: BrukerprofilTjeneste,
-    source: Sequence<Message<String, Siste14aVedtakMelding>>
+    message: Message<String, Siste14aVedtakMelding>
 ) {
-    transaction {
-        source
-            .onEach { message ->
-                appLogger.trace("Mottatt melding: topic=${message.topic} - partition=${message.partition} - offset=${message.offset}")
-            }
-            .mapNotNull { message ->
-                val brukerprofil = message.value.aktorId
-                    ?.let { AktorId(it.value) }
-                    ?.let(idFunction)
-                    ?.let(brukerprofilTjeneste::hentLokalBrukerProfilEllerNull)
-                brukerprofil?.let { it to message }
-            }
-            .filter { (brukerprofil, message) ->
-                val harAktivPeriode = brukerprofil.arbeidssoekerperiodeAvsluttet == null
-                val fattet = message.value.fattetDato?.toInstant()
-                val profileringsTidspunkt = brukerprofil.flagg<HarGodeMuligheterFlagg>()?.tidspunkt ?: Instant.EPOCH
-                val fattetEtterProfilering = fattet != null && fattet.isAfter(profileringsTidspunkt)
-                val harInnsatsgruppe = message.value.innsatsgruppe != null
-                Span.current().addEvent(
-                    "vedtaksfilter", Attributes.of(
-                        booleanKey("vedtak_er_etter_profilering"), fattetEtterProfilering,
-                        booleanKey("har_aktiv_periode"), harAktivPeriode,
-                        booleanKey("har_innsatsgruppe"), harInnsatsgruppe
-                    )
+    message.let { message ->
+        val brukerprofil = message.value.aktorId
+            ?.let { AktorId(it.value) }
+            ?.let(idFunction)
+            ?.let(brukerprofilTjeneste::hentLokalBrukerProfilEllerNull)
+        brukerprofil?.let { it to message }
+    }
+        ?.takeIf { (brukerprofil, message) ->
+            val harAktivPeriode = brukerprofil.arbeidssoekerperiodeAvsluttet == null
+            val fattet = message.value.fattetDato?.toInstant()
+            val profileringsTidspunkt = brukerprofil.flagg<HarGodeMuligheterFlagg>()?.tidspunkt ?: Instant.EPOCH
+            val fattetEtterProfilering = fattet != null && fattet.isAfter(profileringsTidspunkt)
+            val harInnsatsgruppe = message.value.innsatsgruppe != null
+            Span.current().addEvent(
+                "vedtaksfilter", Attributes.of(
+                    booleanKey("vedtak_er_etter_profilering"), fattetEtterProfilering,
+                    booleanKey("har_aktiv_periode"), harAktivPeriode,
+                    booleanKey("har_innsatsgruppe"), harInnsatsgruppe
                 )
-                harAktivPeriode && fattetEtterProfilering && harInnsatsgruppe
-            }
-            .forEach { (profil, melding) ->
-                val brukerId = profil.id
-                val vedtattStdInnsats = melding.value.innsatsgruppe == Innsatsgruppe.STANDARD_INNSATS
-                brukerprofilTjeneste.skrivFlagg(
-                    brukerId,
-                    listOf(StandardInnsatsFlaggtype.flagg(
+            )
+            harAktivPeriode && fattetEtterProfilering && harInnsatsgruppe
+        }
+        ?.run {
+            val (profil, melding) = this
+            val brukerId = profil.id
+            val vedtattStdInnsats = melding.value.innsatsgruppe == Innsatsgruppe.STANDARD_INNSATS
+            brukerprofilTjeneste.skrivFlagg(
+                brukerId,
+                listOf(
+                    StandardInnsatsFlaggtype.flagg(
                         verdi = vedtattStdInnsats,
                         tidspunkt = melding.value.fattetDato!!.toInstant() //Filter på fattetEtterProfilering sikrer non-null her
-                    ))
+                    )
                 )
-            }
-    }
+            )
+        }
 }
