cc9c82b92a0e383d4dc7e373c5a4bdb7a61d9823
Marius Eriksen <marius.eriksen@nav.no>
2026-01-07 11:08:50 +0100

    Husk endring i flyway script :doh:

--- apps/egenvurdering-dialog-tjeneste/src/main/resources/db/migration/V2__dialog_id_nullable_and_dialog_status.sql
+++ apps/egenvurdering-dialog-tjeneste/src/main/resources/db/migration/V2__dialog_id_nullable_and_dialog_status.sql
@@ -1,4 +1,4 @@
 ALTER TABLE periode_id_dialog_id
     ALTER COLUMN dialog_id DROP NOT NULL,
-    ADD COLUMN dialog_status_code VARCHAR(255),
-    ADD COLUMN dialog_error_message VARCHAR(255);
+    ADD COLUMN dialog_status_code INTEGER,
+    ADD COLUMN dialog_error_message TEXT;
