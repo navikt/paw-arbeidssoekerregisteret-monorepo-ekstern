6699f047599cda1d17324eefd33aaf24d635e12f
Thomas Johansen <thomas.johansen3@nav.no>
2026-02-10 10:56:41 +0100

    Lagt til utvidet logging av PDL-feil

--- apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/brukerprofil/beskyttetadresse/HarBeskyttetAdresse.kt
+++ apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/brukerprofil/beskyttetadresse/HarBeskyttetAdresse.kt
@@ -3,48 +3,66 @@ package no.naw.paw.minestillinger.brukerprofil.beskyttetadresse
 import io.opentelemetry.api.common.AttributeKey.longKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.Span
-import no.nav.paw.logging.logger.buildApplicationLogger
 import no.nav.paw.felles.model.Identitetsnummer
+import no.nav.paw.logging.logger.buildApplicationLogger
 import no.nav.paw.pdl.client.PdlClient
 import no.nav.paw.pdl.client.hentAdressebeskyttelse
+import no.nav.paw.pdl.exception.PdlUkjentFeilException
 import no.nav.paw.pdl.graphql.generated.enums.AdressebeskyttelseGradering
 
 const val BEHANDLINGSNUMMER = "B452"
+private val logger = buildApplicationLogger
 
 suspend fun PdlClient.harBeskyttetAdresse(identitetsnummer: Identitetsnummer): Boolean {
-    val adressebeskyttelse = hentAdressebeskyttelse(identitetsnummer.value, null, BEHANDLINGSNUMMER) ?: emptyList()
-    Span.current().addEvent("adressebeskyttelse", Attributes.of(
-        longKey("antall"), adressebeskyttelse.size.toLong())
-    )
-    if (adressebeskyttelse.size > 1) {
-        buildApplicationLogger.warn("Person har flere adressebeskyttelser")
+    try {
+        val adressebeskyttelse = hentAdressebeskyttelse(identitetsnummer.value, null, BEHANDLINGSNUMMER) ?: emptyList()
+        Span.current().addEvent(
+            "adressebeskyttelse", Attributes.of(
+                longKey("antall"), adressebeskyttelse.size.toLong()
+            )
+        )
+        if (adressebeskyttelse.size > 1) {
+            logger.warn("Person har flere adressebeskyttelser")
+        }
+        return adressebeskyttelse.any { it.gradering != AdressebeskyttelseGradering.UGRADERT }
+    } catch (e: PdlUkjentFeilException) {
+        logger.error("Feil ved henting av adressebeskyttelse: ${e.graphQLClientErrors}", e)
+        throw e
     }
-    return adressebeskyttelse.any { it.gradering != AdressebeskyttelseGradering.UGRADERT }
 }
 
-suspend fun PdlClient.harBeskyttetAdresseBulk(identitetsnummer: List<Identitetsnummer>): List<AdressebeskyttelseResultat> =
-    hentAdressebeskyttelse(identitetsnummer.map { it.value }, null, BEHANDLINGSNUMMER)
-        ?.map { personResultat ->
-            val ident = Identitetsnummer(personResultat.ident)
-            val feilkode = personResultat.code.takeUnless { it.equals("ok", ignoreCase = true) }
-            if (feilkode != null) {
-                AdressebeskyttelseFeil(ident, feilkode)
-            } else {
-                val harBeskyttetAdresse = personResultat.person
-                    ?.adressebeskyttelse
-                    ?.any { it.gradering != AdressebeskyttelseGradering.UGRADERT }
-                    ?: false
-                AdressebeskyttelseVerdi(
-                    identitetsnummer = ident,
-                    harBeskyttetAdresse = harBeskyttetAdresse
-                )
-        }
-    } ?: emptyList()
+suspend fun PdlClient.harBeskyttetAdresseBulk(identitetsnummer: List<Identitetsnummer>): List<AdressebeskyttelseResultat> {
+    try {
+        return hentAdressebeskyttelse(identitetsnummer.map { it.value }, null, BEHANDLINGSNUMMER)
+            ?.map { personResultat ->
+                val ident = Identitetsnummer(personResultat.ident)
+                val feilkode = personResultat.code.takeUnless { it.equals("ok", ignoreCase = true) }
+                if (feilkode != null) {
+                    AdressebeskyttelseFeil(ident, feilkode)
+                } else {
+                    val harBeskyttetAdresse = personResultat.person
+                        ?.adressebeskyttelse
+                        ?.any { it.gradering != AdressebeskyttelseGradering.UGRADERT }
+                        ?: false
+                    AdressebeskyttelseVerdi(
+                        identitetsnummer = ident,
+                        harBeskyttetAdresse = harBeskyttetAdresse
+                    )
+                }
+            } ?: emptyList()
+    } catch (e: PdlUkjentFeilException) {
+        logger.error("Feil ved henting av adressebeskyttelse: ${e.graphQLClientErrors}", e)
+        throw e
+    }
+}
 
 sealed interface AdressebeskyttelseResultat {
     val identitetsnummer: Identitetsnummer
 }
 
-data class AdressebeskyttelseVerdi(override val identitetsnummer: Identitetsnummer, val harBeskyttetAdresse: Boolean): AdressebeskyttelseResultat
-data class AdressebeskyttelseFeil(override val identitetsnummer: Identitetsnummer, val code: String): AdressebeskyttelseResultat
+data class AdressebeskyttelseVerdi(override val identitetsnummer: Identitetsnummer, val harBeskyttetAdresse: Boolean) :
+    AdressebeskyttelseResultat
+
+data class AdressebeskyttelseFeil(override val identitetsnummer: Identitetsnummer, val code: String) :
+    AdressebeskyttelseResultat
 
