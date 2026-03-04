3ee2d760bbcc29da4770639bbfea7e4442ccd73d
Nils Martin Sande <nils.martin.sande@nav.no>
2026-03-04 14:40:52 +0100

    La til styrk mapping fra arbeidsplassen.no

--- apps/mine-stillinger-api/src/main/kotlin/no/naw/paw/minestillinger/brukerprofil/ABTesting.kt
+++
@@ -1,8 +0,0 @@
-package no.naw.paw.minestillinger.brukerprofil
-
-import no.nav.paw.felles.model.Identitetsnummer
-
-fun sjekkABTestingGruppe(regex: Regex, identitetsnummer: Identitetsnummer): Boolean {
-    return regex.matches(identitetsnummer.value)
-}
-
