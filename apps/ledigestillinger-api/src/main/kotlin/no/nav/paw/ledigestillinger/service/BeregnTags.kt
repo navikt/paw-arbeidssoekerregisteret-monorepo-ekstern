f68ae59fb1299ad853ea18634ddc826ff86d01f2
Nils Martin Sande <nils.martin.sande@nav.no>
2026-03-06 09:56:10 +0100

    La til span hendelse for beregning av tags

--- apps/ledigestillinger-api/src/main/kotlin/no/nav/paw/ledigestillinger/service/BeregnTags.kt
+++ apps/ledigestillinger-api/src/main/kotlin/no/nav/paw/ledigestillinger/service/BeregnTags.kt
@@ -1,5 +1,8 @@
 package no.nav.paw.ledigestillinger.service
 
+import io.opentelemetry.api.common.AttributeKey.stringKey
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.trace.Span
 import no.nav.pam.stilling.ext.avro.Ad
 
 fun beregnTags(ad: Ad): Set<Tag> {
@@ -11,5 +14,16 @@ fun beregnTags(ad: Ad): Set<Tag> {
         hashSetOf(Tag.DIREKTEMELDT_V1)
     } else {
         emptySet()
+    }.also { tags ->
+        Span.current()
+            .addEvent("beregnet_tags",
+                Attributes.of(
+                    stringKey("source"), ad.source,
+                    stringKey("direktemeldtStillingskategori"), ad.properties.find { prop ->
+                        prop.key.equals("direktemeldtStillingskategori", ignoreCase = true)
+                    }?.value ?: "null",
+                    stringKey("tags"), tags.joinToString(",")
+                )
+            )
     }
 }
